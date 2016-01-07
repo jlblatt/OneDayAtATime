d0f8f11ead65aa4d3725db8ac5a86b955533a3bd
Jason Blatt <jlblatt+github@gmail.com>
2016-01-07 11:27:32 -0700

    Chart.js

--- index.php
+++ index.php
@@ -5,15 +5,15 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>OneDayAtATime</title>
-    <link href="/css/styles.css" rel="stylesheet">
+    <link href="css/styles.css" rel="stylesheet">
     <script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
-    <script src="//d3js.org/d3.v3.min.js" charset="utf-8"></script>
-    <script src="/js/scripts.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.min.js"></script>
+    <script src="js/scripts.js"></script>
   </head>
 
   <body>
 
-    <div id="graph"></div>
+    <canvas id="chart"></canvas>
 
   </body>
 
--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,7 @@
 
 $(document).ready(function(){
 
+  var ctx = document.getElementById("chart").getContext("2d");
+  var chart = new Chart(ctx);
+
 });
