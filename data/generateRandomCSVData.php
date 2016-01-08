197472961be1f04f77312f1310d4250a2e90eda1
Jason Blatt <jlblatt+github@gmail.com>
2016-01-08 11:19:57 -0700

    Better data model

--- data/generateRandomCSVData.php
+++ data/generateRandomCSVData.php
@@ -3,11 +3,10 @@
 for($i = 0; $i < 90; $i++)
 {
 	$d = $i;
-  //$d = $i + mt_rand(-1, 1);
-  //if($d < 0) $d = 0;
+  $d = $i + mt_rand(-1, 1);
+  if($d < 0) $d = 0;
   echo date("n/j/Y", strtotime("today - 1 year + " . $d . " days"));
   echo ",";
-	//  echo mt_rand(0, 18);
-	echo $i;
+	echo mt_rand(0, 4);
   echo "\n";
 }
--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,58 @@
 "use strict";
 
+var ONE_DAY = 86400000;
+
 function drawChart(csv)
 {
 		
 		//parse the csv
 		var csv = csv.split("\n");
-		while(csv[0] == "") csv.shift();
-		while(csv[csv.length - 1] == "") csv.pop();
-		
-		var sd = Date.parse(csv[0].split(',')[0]);
-		var ed = Date.parse(csv[csv.length - 1].split(',')[0]);
-		
-		var d_daily = {}, d_weekly = {}, d_monthly = {};
-		
-		//populate daily
-		for(var d = sd; d < ed; d += 86400000)
+		for(var i = 0; i < csv.length; i++)
 		{
-				if(!(d in d_daily)) d_daily[d] = 0;
-				
-				while(csv.length > 0)
+				if(!csv[i].match(/^\d{1,2}\/\d{1,2}\/\d{4}\,[0-9\.]+$/))
 				{
-						if(d >= Date.parse(csv[0].split(',')[0])) 
-						{
-								d_daily[d] += parseInt(csv[0].split(',')[1]);
-								csv.shift();
-						}
-						
-						else break;
+						csv.splice(i, 1);
+						i--;
 				}
 		}
 
+		//populate daily
+		var d_daily = [];
+		for(var i = 0; i < csv.length; i++)
+		{
+				var entry = csv[i].split(',');
+				
+				var thisDate = Date.parse(entry[0]);
+				var thisCount = parseInt(entry[1]);
+
+				if(d_daily.length > 0)
+				{
+						var lastIndex = d_daily.length - 1;
+						var currDate = d_daily[lastIndex][0];
+						var currentAggregate = d_daily[lastIndex][1];
+
+						if(thisDate == currDate) d_daily[lastIndex][1] += thisCount;
+						else d_daily.push([thisDate, thisCount]);
+
+				}
+
+				else d_daily.push([thisDate, thisCount]);
+
+		}
+		
 		//populate weekly
 
 		//populate monthly
 
+
+
 		//chart daily
 		var data = { labels: [], datasets: [] };
 
-		for(var i in d_daily) data.labels.push('');
+		for(var i = 0; i < d_daily.length; i++) data.labels.push('');
 		data.datasets.push({
 				label: "Daily",
+				bezierCurve : false,
 				fillColor: "rgba(220,220,220,0.2)",
 				strokeColor: "rgba(220,220,220,1)",
 				pointColor: "rgba(220,220,220,1)",
@@ -48,14 +61,16 @@ function drawChart(csv)
 				pointHighlightStroke: "rgba(220,220,220,1)",
 				data: []
 		});
-		for(var i in d_daily) data.datasets[0].data.push(d_daily[i]);
-
-		var ctx = document.getElementById("chart").getContext("2d");
-		var chart = new Chart(ctx).Line(data);
+		for(var i = 0; i < d_daily.length; i++) data.datasets[0].data.push(d_daily[i][1]);
 
 		//chart weekly
 
 		//chart monthly
+
+		//render
+
+		var ctx = document.getElementById("chart").getContext("2d");
+		var chart = new Chart(ctx).Line(data);
 		
 }
 
